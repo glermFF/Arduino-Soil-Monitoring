ae337ec87e75c6194aa41c959e65e8c904df0152
Guilherme Ferreira Freitas <87777795+glermFF@users.noreply.github.com>
2024-08-03 22:22:11 -0300

    Corrigindo erros

--- vaso_iot.ino
+++ vaso_iot.ino
@@ -17,34 +17,51 @@ float umidade;
 float porcenUmidade;
 
 void setup() {
-  //Serial.begin(9600);
+  Serial.begin(9600);
   Wire.begin();
   OLED.begin(SSD1306_SWITCHCAPVCC, 0X3C);
   pinMode(pinA, INPUT);
 }
 
-void loop() {
-  umidade = analogRead(pinA); // Valor da umidade
-  porcenUmidade = map(umidade, 1023.0, 0.0, 0.0, 100.0); // Define a umidade em porcentagem
+int GetUmidade(){
+  umidade = analogRead(pinA); // Entrada analogica para pegar valor da umidade 
+  porcenUmidade = map(umidade, 1024.0, 316.0, 0.0, 100.0); // Define a umidade em porcentagem
+  return porcenUmidade;
+}
 
+void header(){
+  umidade = GetUmidade();
+  OLED.clearDisplay();
+  OLED.setTextSize(1);
+  OLED.setTextColor(WHITE);
+  OLED.setCursor(0, 1);
+
+  // Retorna Status sobre o solo
+  if (porcenUmidade >= 0.0 && porcenUmidade <= 25.0){
+    OLED.println("Status da planta: Solo Seco");
+    OLED.display();
+  } else if (porcenUmidade > 25.0 && porcenUmidade <= 75.0){
+    OLED.println("Status da planta: Precisando Regar");
+    OLED.display();
+  } else if (porcenUmidade > 75.0 && porcenUmidade <= 100.0){
+    OLED.println("Status da planta: Solo Umido");
+    OLED.display();
+  }
+  
+}
+
+void loop() {
+  umidade = GetUmidade();
+  
   // Configurações de exibição do display
   OLED.clearDisplay();
   OLED.setTextSize(1);
   OLED.setTextColor(WHITE);
-  OLED.setCursor(0, 20);
+  OLED.setCursor(0, 10);
+
+  header();
   
-  // Informações do solo
-  if (porcenUmidade >= 0.0 && porcenUmidade <= 25.0){
-    OLED.print("Status da planta: Solo Seco");
-    OLED.println("Umidade(%): ");
-    OLED.print(porcenUmidade);
-  } else if (porcenUmidade > 25.0 && porcenUmidade <= 50.0){
-    OLED.print("Status da planta: Precisando Regar");
-    OLED.println("Umidade(%): ");
-    OLED.print(porcenUmidade);
-  } else if (porcenUmidade > 50.0 && porcenUmidade <= 100.0){
-    OLED.print("Status da planta: Solo Umido");
-    OLED.println("Umidade(%): ");
-    OLED.print(porcenUmidade);
-  }
+  OLED.println("Umidade(%): ");
+  OLED.print( umidade );
+  OLED.display();
 }
